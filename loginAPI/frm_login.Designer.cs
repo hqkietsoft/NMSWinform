77b16b61da6f992ff8f5aa5492142f4a17207259
DAFF <hagiakiet.work@gmail.com>
2024-12-19 09:13:19 +0700

    the first

---
+++ loginAPI/frm_login.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace loginAPI
+{
+    partial class frm_Login_out
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.rtbInfo = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(28, 233);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(110, 33);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(28, 287);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(110, 30);
+            this.btnLogout.TabIndex = 1;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(10, 189);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(34, 16);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "ABC";
+            // 
+            // rtbInfo
+            // 
+            this.rtbInfo.Location = new System.Drawing.Point(178, 25);
+            this.rtbInfo.Name = "rtbInfo";
+            this.rtbInfo.Size = new System.Drawing.Size(537, 499);
+            this.rtbInfo.TabIndex = 3;
+            this.rtbInfo.Text = "";
+            // 
+            // frm_Login_out
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 554);
+            this.Controls.Add(this.rtbInfo);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "frm_Login_out";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login & Logout";
+            this.Load += new System.EventHandler(this.frm_Login_out_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.RichTextBox rtbInfo;
+    }
+}
+
---
+++ loginAPI/frm_login.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace loginAPI
+{
+    public partial class frm_Login_out : Form
+    {
+        private string clientId = "NMS_Mobile";
+        private string issuer = "https://apinpm.egov.phutho.vn";
+        private string redirectUri = "http://localhost:4000";
+        private string accessToken;
+        private string logoutUrl;
+        private DateTime tokenExpiryTime;
+
+        public frm_Login_out()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_Login_out_Load(object sender, EventArgs e)
+        {
+            // Khởi tạo trạng thái ban đầu
+            lblStatus.Text = "Chưa đăng nhập";
+            rtbInfo.Text = "Thông tin mã và token sẽ hiển thị tại đây.\n";
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    // Khám phá issuer
+                    var discoveryUrl = $"{issuer}/.well-known/openid-configuration";
+                    var discoveryResponse = await client.GetStringAsync(discoveryUrl);
+                    var discoveryData = JObject.Parse(discoveryResponse);
+                    var authEndpoint = discoveryData["authorization_endpoint"].ToString();
+                    var tokenEndpoint = discoveryData["token_endpoint"].ToString();
+
+                    // Redirect user to login
+                    var authUrl = $"{authEndpoint}?client_id={clientId}&redirect_uri={redirectUri}&response_type=code&scope=openid profile email offline_access";
+                    System.Diagnostics.Process.Start(authUrl);
+
+                    // Nhận mã code qua callback
+                    string code = await StartLocalHttpServer();
+                    rtbInfo.AppendText($"Mã nhận được: {code}\n");
+
+                    // Trao đổi code để lấy token
+                    var tokenResponse = await client.PostAsync(tokenEndpoint, new FormUrlEncodedContent(new[]
+                    {
+                        new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                        new KeyValuePair<string, string>("code", code),
+                        new KeyValuePair<string, string>("redirect_uri", redirectUri),
+                        new KeyValuePair<string, string>("client_id", clientId)
+                    }));
+
+                    tokenResponse.EnsureSuccessStatusCode();
+
+                    var tokenData = JObject.Parse(await tokenResponse.Content.ReadAsStringAsync());
+                    accessToken = tokenData["access_token"].ToString();
+                    int expiresIn = tokenData["expires_in"].ToObject<int>();
+                    tokenExpiryTime = DateTime.Now.AddSeconds(expiresIn);
+
+                    rtbInfo.AppendText($"Access Token: {accessToken}\n");
+                    rtbInfo.AppendText($"Token Expiry Time: {tokenExpiryTime}\n");
+
+                    // Cập nhật trạng thái
+                    lblStatus.Text = "Đăng nhập thành công!";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi đăng nhập: {ex.Message}");
+            }
+        }
+
+        private async void btnLogout_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(logoutUrl))
+            {
+                MessageBox.Show("Bạn chưa đăng nhập!");
+                return;
+            }
+
+            try
+            {
+                // Redirect user to logout
+                System.Diagnostics.Process.Start(logoutUrl);
+                accessToken = null;
+                logoutUrl = null;
+                lblStatus.Text = "Đã đăng xuất.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi đăng xuất: {ex.Message}");
+            }
+        }
+
+        private async Task<string> StartLocalHttpServer()
+        {
+            using (HttpListener listener = new HttpListener())
+            {
+                listener.Prefixes.Add("http://localhost:4000/");
+                listener.Start();
+                Console.WriteLine("Listening for callbacks...");
+
+                // Chờ request từ OpenID Connect provider
+                HttpListenerContext context = await listener.GetContextAsync();
+                string code = context.Request.QueryString["code"]; // Lấy mã code từ query string
+
+                // Gửi phản hồi tới trình duyệt
+                string responseString = "<html><body>Login successful. You can close this window.</body></html>";
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                context.Response.ContentLength64 = buffer.Length;
+                context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                context.Response.OutputStream.Close();
+
+                listener.Stop();
+                return code;
+            }
+        }
+    }
+}
